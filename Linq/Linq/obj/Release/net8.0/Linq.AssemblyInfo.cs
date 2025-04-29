316fe5fedcf98db6a0f16fdb350f968ae030e2cb
Ezik143 <jakesucgang90@gmail.com>
2025-04-29 13:44:17 +0800

    added documentation comments

--- Linq/Linq/obj/Release/net8.0/Linq.AssemblyInfo.cs
+++ Linq/Linq/obj/Release/net8.0/Linq.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Linq")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+638b07390571b695e6ac4d33313a11252303ac8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Linq")]
--- Linq/TestProject1/obj/Debug/net9.0/TestProject1.AssemblyInfo.cs
+++ Linq/TestProject1/obj/Debug/net9.0/TestProject1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81f37d1cea9714505bcf381b0ae62ffffd00d948")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+638b07390571b695e6ac4d33313a11252303ac8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
